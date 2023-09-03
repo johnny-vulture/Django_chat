cbd41fccc40407c04d8b4511fa4d2f8210f870c6
johnny-vulture <aaronmamutse@gmail.com>
2023-09-03 09:01:36 +0200

    added rooms

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/rooms/'
+LOGIN_URL = '/login/'
 
 # Application definition
 
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'core',
+    'room',
 ]
 
 MIDDLEWARE = [
